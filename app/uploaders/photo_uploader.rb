f4fb24b663deb2d797ef39148947ddc9ffd439e4
Yves Sagnieres <sagnieres.yves@gmail.com>
2018-02-23 15:36:27 -0500

    tweaked uploade

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,4 +3,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   process eager: true  # Force version generation at upload time.
 
   process convert: 'jpg'
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 end
